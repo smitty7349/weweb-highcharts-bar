e03d1eef76781e52421e9c1449a123c3a9837911
Chase Macbook Air <chadonsom@gmail.com>
2024-06-01 10:26:24 -0400

    add a series config option

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -18,37 +18,29 @@ const mainRef = ref(null)
  * @type {{ value: Highcharts.Chart|null }}
  */
 const chart = ref(null)
-onMounted(() => {
-  console.log("mainRef.value :", mainRef.value)
-  chart.value = Highcharts.chart(mainRef.value, {
-    chart: {
-      type: "bar",
-    },
+const series = ref(props.content.series || [])
+const defaultHighchartsOptions = ref({
+  chart: {
+    type: "bar",
+  },
+  title: {
+    text: props.content.title,
+  },
+  subtitle: {
+    text: props.content.subtitle,
+  },
+  xAxis: {
+    categories: ["Apples", "Bananas", "Oranges"],
+  },
+  yAxis: {
     title: {
-      text: props.content.title,
+      text: "Fruit eaten",
     },
-    subtitle: {
-      text: props.content.subtitle,
-    },
-    xAxis: {
-      categories: ["Apples", "Bananas", "Oranges"],
-    },
-    yAxis: {
-      title: {
-        text: "Fruit eaten",
-      },
-    },
-    series: [
-      {
-        name: "Jane",
-        data: [1, 0, 4],
-      },
-      {
-        name: "John",
-        data: [5, 7, 3],
-      },
-    ],
-  })
+  },
+  series,
+})
+onMounted(() => {
+  chart.value = Highcharts.chart(mainRef.value, defaultHighchartsOptions.value)
 })
 
 const chartTitle = computed(() => props.content.title)
@@ -60,4 +52,16 @@ const chartSubtitle = computed(() => props.content.subtitle)
 watch(chartSubtitle, (newVal) => {
   chart.value?.subtitle.update({ text: newVal })
 })
+
+const chartSeries = computed(() => props.content.series)
+watch(
+  chartSeries,
+  (newVal) => {
+    chart.value = Highcharts.chart(mainRef.value, {
+      ...defaultHighchartsOptions.value,
+      series: newVal,
+    })
+  },
+  { deep: true }
+)
 </script>
--- ww-config.js
+++ ww-config.js
@@ -21,5 +21,34 @@ export default {
       defaultValue: "by fruit",
       responsive: true,
     },
+    series: {
+      label: {
+        en: "Series",
+      },
+      defaultValue: [
+        {
+          name: "Jane",
+          data: [1, 0, 4],
+        },
+        {
+          name: "John",
+          data: [5, 7, 3],
+        },
+      ],
+      type: "Info",
+      options: {
+        text: "Bind series data",
+      },
+      bindable: "list",
+      responsive: true,
+      states: true,
+      section: "settings",
+      /* wwEditor:start */
+      bindingValidation: {
+        type: "array",
+        tooltip: "A collection of series data in array format: `[{ name: 'Jane', data: [0, 1, 2] }, {...}, ...]`",
+      },
+      /* wwEditor:end */
+    },
   },
 }
