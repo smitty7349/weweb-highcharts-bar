7e61294a2c5bf9236d03791ec17658d2b8905263
Chase Macbook Air <chadonsom@gmail.com>
2024-06-03 20:26:19 -0400

    fix bug having only one chart

--- src/composables/useChart.js
+++ src/composables/useChart.js
@@ -1,21 +1,21 @@
 import Highcharts from "highcharts"
 import { ref } from "vue"
 
-/**
- * @type {{ value: Highcharts.Chart|null }}
- */
-const chart = ref(null)
-
-function refreshChart(mainRef, options = {}) {
-  try {
-    chart.value?.destroy()
-  } catch (e) {
-    console.error(e)
-  }
-  chart.value = Highcharts.chart(mainRef.value, options)
-}
-
 export default function useChart(mainRef) {
+  /**
+   * @type {{ value: Highcharts.Chart|null }}
+   */
+  const chart = ref(null)
+
+  function refreshChart(mainRef, options = {}) {
+    try {
+      chart.value?.destroy()
+    } catch (e) {
+      console.error(e)
+    }
+    chart.value = Highcharts.chart(mainRef.value, options)
+  }
+
   return {
     chart,
     refreshChart: refreshChart.bind(null, mainRef),
